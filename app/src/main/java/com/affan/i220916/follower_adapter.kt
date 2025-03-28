ef8d6e1bc93930ba5345d0895ed4b59b8dc0c72d
Adil Nadeem <114493582+adilnadeem02@users.noreply.github.com>
2025-03-28 19:25:14 +0500

    Feature/dm from tabs (#23)
    * feat: DM's from Follower and Following Tab

--- app/src/main/java/com/affan/i220916/follower_adapter.kt
+++ app/src/main/java/com/affan/i220916/follower_adapter.kt
@@ -1,10 +1,12 @@
 package com.affan.i220916
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class follower_adapter(private val items: List<follower_model>) : RecyclerView.Adapter<follower_adapter.followerViewHolder>() {
@@ -12,6 +14,7 @@ class follower_adapter(private val items: List<follower_model>) : RecyclerView.A
     class followerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val profileImage: ImageView = itemView.findViewById(R.id.pfp)
         val userName: TextView = itemView.findViewById(R.id.username)
+        val message: ImageView = itemView.findViewById(R.id.msg)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): followerViewHolder {
@@ -24,6 +27,13 @@ class follower_adapter(private val items: List<follower_model>) : RecyclerView.A
         val item = items[position]
         holder.profileImage.setImageBitmap(item.profileImageResId)
         holder.userName.text = item.userName
+
+        holder.message.setOnClickListener {
+            var intent = Intent(holder.itemView.context, dm::class.java)
+            intent.putExtra("userId", item.userId)
+            var context = holder.itemView.context
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = items.size
